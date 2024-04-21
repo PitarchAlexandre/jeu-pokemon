bf0f387e29eb9ef6af1a8504292569c115cea1dc
Alex <alexandre.pitarch@divcom.ch>
2024-04-22 00:36:08 +0200

    optimisation design

--- javascript/pokemon.js
+++ javascript/pokemon.js
@@ -633,7 +633,7 @@ function choixAttaqueBot(pokemonAdversaire){
     console.log(puissanceAttAdversaire);
     //retourne les points de puissance d'attaque à l'eventListener
     return puissanceAttAdversaire;
-}
+};
 //Fonction qui permet de désactiver le pokémon et l'attaque lorsqu'il n'a plus de PV
 function disableAttackButtons(pokemonIndex) {
     // Désactive les boutons d'attaque du Pokémon correspondant
